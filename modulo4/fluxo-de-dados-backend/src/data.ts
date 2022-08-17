c6722b49cf5fa81fd07ab46d94220e8b59c2eb09
Giovanna Magalhães <giovannamglhs06@gmail.com>
2022-08-17 18:17:12 -0300

    Fluxo de Dados - Backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,27 @@
+//EXERCÍCIO 2
+
+export type Produto = {
+  id: number,
+  name: string,
+  price: number
+};
+
+export const arrayDeProdutos: Produto[] = [
+  {
+    id: 1,
+    name: "Detergente",
+    price: 4,
+  },
+  {
+    id: 2,
+    name: "Sabão em pó",
+    price: 13
+  },
+  {
+    id: 3,
+    name: "Água Sanitária",
+    price: 5
+  }
+];
+
+
